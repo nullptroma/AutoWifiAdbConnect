0e672c3efdbedac1bda623979e8976a31977d756
Electrominch <64524860+Electrominch@users.noreply.github.com>
2021-10-08 13:23:14 +0300

    AllIps Fix

--- AutoWifiAdbConnect/LocalNetworkTools/ARP.cs
+++ AutoWifiAdbConnect/LocalNetworkTools/ARP.cs
@@ -1,5 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
+using System.Windows;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AutoWifiAdbConnect.LocalNetworkTools
 {
@@ -7,20 +13,20 @@ namespace AutoWifiAdbConnect.LocalNetworkTools
     {
         private void fillCache()
         {
+            string myIp;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                myIp = endPoint.Address.ToString();
+            }
+            string pattern = myIp.Substring(0, myIp.LastIndexOf('.')) + @".%a";
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.CreateNoWindow = true;
             startInfo.UseShellExecute = false;
-            startInfo.FileName = "cmd";
-            startInfo.Arguments = @"/c " + @"for /L %a in (1,1,254) do @start /b ping 192.168.1.%a -n 1 > nul";
+            startInfo.FileName = $"cmd";
+            startInfo.Arguments = @"/c " + $@"for /L %a in (1,1,254) do @start /b ping {pattern} -n 2 > nul";
             Process process = Process.Start(startInfo);
-            process.WaitForExit();
-
-            startInfo = new ProcessStartInfo();
-            startInfo.CreateNoWindow = true;
-            startInfo.UseShellExecute = false;
-            startInfo.FileName = "cmd";
-            startInfo.Arguments = @"/c " + @"for /L %a in (1,1,254) do @start /b ping 192.168.0.%a -n 1 > nul";
-            process = Process.Start(startInfo);
             process.WaitForExit();
         }
 
